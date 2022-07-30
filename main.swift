2f70c3efb901f7029f366b51f46b9b6e32279d26
Nissi <109863086+nissinissinissi@users.noreply.github.com>
2022-07-30 04:23:48 -0500

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,53 @@
+//
+//  main.swift
+//  3d hw
+//
+//  Created by User on 2 Av 5782.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var com  = 100000
+var percentage = 0.24
+var salary = 20000
+var totalString = ""
+
+
+func countTotal( money: String) {
+ 
+
+    }
+    var increase = 2000
+    
+
+func countAll (){
+    
+    print ("\(totalString) общая сумма \(com). проценты  24% - \(percentage *  0.24) + \(Double(com) * 0.24))")
+}
+
+
+func count (){
+   
+    var monthly = readLine()!
+     countTotal(money: monthly)
+
+}
+print ("Изначальная сумма кредита : 1000000 , процент = 24 % , каждый год - 1 %")
+print (" Общий долг = \(com) * \(percentage )= 2400000")
+print  ( "Каждый год - 1 %  = 4 % - \(240000000) - \(96000) = 2304000) " )
+
+
+func total (){}
+print (" Зарплата - \(salary) * \( 50 ) + \(2000) * \(25) = 105000 ")
+print (" процент за 4 года\(24) - \(4 ) = 20%)")
+print ("за 4 года и 3 месяца ежемесячно - 43980")
+
+
+
+
+
+
+
+
